a37a3b66a01bea5492a94e1d1c05698d415fb7f0
Lior Kuperiu <kuperiu@gmail.com>
2022-06-30 08:44:15 +0200

    test

--- Formula/telepresence.rb
+++ Formula/telepresence.rb
@@ -6,6 +6,17 @@ class Telepresence < Formula
   url "https://app.getambassador.io/download/tel2/darwin/amd64/2.6.8/telepresence"
   sha256 "d215a3c3c593e638d326643336b478892b1357a316dd9606c804ea3cb107bee8"
 
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://app.getambassador.io/download/tel2/darwin/amd64/2.6.8/telepresence"
+      sha256 "d215a3c3c593e638d326643336b478892b1357a316dd9606c804ea3cb107bee8"
+    end
+    if Hardware::CPU.arm?
+      url "https://app.getambassador.io/download/tel2/darwin/arm64/2.6.8/telepresence"
+      sha256 "0a5561a113b732a512430067abe39e783e5e19eeab2151e89181e8cee260b3eb"
+    end
+  end
+
   # macfuse is a cask and formula can't depend on casks, so we can't actually
   # do this. This is probably fine since you don't _need_ macfuse to run
   # the cli, just to do mounts 
